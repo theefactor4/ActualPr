d289f496b9884adb9d58e8c3f572f5a219ccc717
theefactor4 <support@adbgames.net>
2025-02-24 11:30:07 +0000

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -41,8 +41,7 @@ export const routes: Routes = [
     {path:'shop-details/:id',component:ShopDetailsComponent,title:"Shop Single - Mykd"},
     {path:'reset-password',component:ResetPasswordComponent,title:"Reset Password - Mykd"},
     {path:'about',component:AboutComponent,title:"About - Mykd"},
-    {path:'blog',component:BlogComponent,title:"Blog - Mykd"},
-    {path:'blog-details/:id',component:BlogDetailsComponent,title:'Blog Details - Mykd'},
+ 
     {path:'contact',component:ContactComponent,title:"Contact - Mykd"},
     {path:'**',component:NotFoundComponent,title:"404 - Mykd"},
 ];
